1c37ec0f4b5390ead2ac2835cb1b244148f134fa
DesiredDesire <konrad.wierzbik@gmail.com>
2024-08-05 14:54:41 +0200

    rename contracts

--- src/contracts/abax_governor/modules/govern/traits/mod.rs
+++ src/contracts/abax_governor/modules/govern/traits/mod.rs
@@ -8,10 +8,7 @@ pub use structs::*;
 
 pub use pendzl::traits::String;
 
-pub use ink::{
-    prelude::{string::ToString, vec::Vec},
-    primitives::Hash,
-};
+pub use ink::{prelude::vec::Vec, primitives::Hash};
 
 include!("govern.trait.rs");
 include!("govern_manage.trait.rs");
